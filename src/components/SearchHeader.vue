70968673ae9f5791545a645bd631962f3961378d
yckim-lab <yc01000@hanmail.net>
2022-11-04 00:28:10 +0900

    date picker 1

--- src/components/SearchHeader.vue
+++ src/components/SearchHeader.vue
@@ -1,78 +1,71 @@
 <template>
-  <!-- <div class="hello">
-    <div>
-      <b-dropdown id="dropdown-1" text="Dropdown Button" class="m-md-2">
-        <b-dropdown-item>First Action</b-dropdown-item>
-        <b-dropdown-item>Second Action</b-dropdown-item>
-        <b-dropdown-item>Third Action</b-dropdown-item>
-      </b-dropdown>
-    </div>
-    <b-button @click='clickParams' variant="outline-primary">{{ msg }}</b-button>
-  </div> -->
   <div>
-        <b-navbar toggleable="lg" type="light" variant="secondary">
-            <b-navbar-brand href="#" ><img src="@/assets/top_logo.gif" width="150" height="30" class="d-inline-block align-top" alt="Kitten">
-              진마켓 판매실적
-            </b-navbar-brand>
+    <b-navbar toggleable="lg" type="light" variant="secondary">
+      <b-navbar-brand href="#"><img src="@/assets/top_logo.gif" width="150" height="30" class="d-inline-block align-top" alt="Kitten">
+        진마켓 판매실적
+      </b-navbar-brand>
+      <Datepicker v-model="date" multiDates placeholder="Start Typing ..." textInput @textSubmit="alertDate" v-bind:textInputOptions="textInputOptions" />
 
-            <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+      <h3>~</h3>
+      <Datepicker v-model="time" timePicker modeHeight="120">
+        <template #dp-input="{ value, }">
+          <input type="text" :value="value"/>
+        </template>
+      </Datepicker>
+      <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-            <b-collapse id="nav-collapse" is-nav>
-                <!-- Right aligned nav items -->
-                <b-navbar-nav class="ml-auto">
-
-                    <b-nav-item-dropdown text="Lang" right>
-                    <b-dropdown-item href="#">EN</b-dropdown-item>
-                    <b-dropdown-item href="#">ES</b-dropdown-item>
-                    <b-dropdown-item href="#">RU</b-dropdown-item>
-                    <b-dropdown-item href="#">FA</b-dropdown-item>
-                    </b-nav-item-dropdown>
-                    <div class="row">
-                      <div class="col">
-                        <input type="text" class="form-control" placeholder="First name" aria-label="First name">
-                      </div>
-                      <div class="col">
-                        <input type="text" class="form-control" placeholder="Last name" aria-label="Last name">
-                      </div>
-                    </div>
-                    <b-nav-item-dropdown right>
-                      <!-- Using 'button-content' slot -->
-                      <template #button-content>
-                          <em>User</em>
-                      </template>
-                      <b-dropdown-item href="#">Profile</b-dropdown-item>
-                      <b-dropdown-item href="#">Sign Out</b-dropdown-item>
-                    </b-nav-item-dropdown>
-
-                    <b-nav-form>
-                    <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
-                    <!-- <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button> -->
-                    <b-button @click='clickParams' variant="danger" size="sm" class="my-2 my-sm-0">{{ msg }}</b-button>
-                    </b-nav-form>
-
-                </b-navbar-nav>
-            </b-collapse>
-        </b-navbar>
-    </div>
-
-
+      <b-collapse id="nav-collapse" is-nav>
+        <!-- Right aligned nav items -->
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <!-- <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input> -->
+            <b-button @click='clickParams' variant="danger" size="sm" class="my-2 my-sm-0">{{ msg }}</b-button>
+          </b-nav-form>
+        </b-navbar-nav>
+      </b-collapse>
+    </b-navbar>
+  </div>
 </template>
 
 <script>
 import axios from 'axios'
+import Datepicker from '@vuepic/vue-datepicker'
+import '@vuepic/vue-datepicker/dist/main.css'
+import { ref } from 'vue';
 
 export default {
   props: {
     msg: String
   },
-
+  components: { Datepicker },
   data: function () {
     return {
+
     }
   },
+  setup() {
+    const date = ref();
+    const textInputOptions = ref({
+      format: 'yyyy.MM.dd',
+    });
+    const time = ref({
+      hours: new Date().getHours(),
+      minutes: new Date().getMinutes(),
+    });
+    const alertDate = () => {
+          alert(date.value);
+        };
 
+    return {
+      date,
+      textInputOptions,
+      time,
+      alertDate,
+    }
+  },
   methods: {
     clickParams: async function () {
+      console.log("textInputOptions ", this.textInputOptions );
       var resdata;
       var per_resdata;
       await axios.get('server/get_chart_data2.jsp').then((response) => {
