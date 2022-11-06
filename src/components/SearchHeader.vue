82dca088cc590bea5fc0bf81b07146d7f67cc9ae
yckim-lab <yc01000@hanmail.net>
2022-11-07 01:15:07 +0900

    datepicker1

--- src/components/SearchHeader.vue
+++ src/components/SearchHeader.vue
@@ -1,28 +1,23 @@
 <template>
   <div>
     <b-navbar toggleable="lg" type="light" variant="secondary">
-      <b-navbar-brand href="#"><img src="@/assets/top_logo.gif" width="150" height="30" class="d-inline-block align-top" alt="Kitten">
+      <b-navbar-brand href="#"><img src="@/assets/top_logo.gif" width="150" height="30" class="d-inline-block align-top"
+          alt="Kitten">
         진마켓 판매실적
       </b-navbar-brand>
-      <Datepicker v-model="date" multiDates placeholder="Start Typing ..." textInput @textSubmit="alertDate" v-bind:textInputOptions="textInputOptions" />
-
+      <Datepicker v-model="date" multiDates :format="format" />
       <h3>~</h3>
       <Datepicker v-model="time" timePicker modeHeight="120">
-        <template #dp-input="{ value, }">
-          <input type="text" :value="value"/>
-        </template>
+        <!-- <template #dp-input="{ value, }">
+          <input type="text" :value="value" />
+        </template> -->
       </Datepicker>
       <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
 
-      <b-collapse id="nav-collapse" is-nav>
-        <!-- Right aligned nav items -->
-        <b-navbar-nav class="ml-auto">
-          <b-nav-form>
-            <!-- <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input> -->
-            <b-button @click='clickParams' variant="danger" size="sm" class="my-2 my-sm-0">{{ msg }}</b-button>
-          </b-nav-form>
-        </b-navbar-nav>
-      </b-collapse>
+      <b-nav-form>
+        <!-- <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input> -->
+        <b-button @click='clickParams' variant="danger" size="sm" class="my-2 my-sm-0">{{ msg }}</b-button>
+      </b-nav-form>
     </b-navbar>
   </div>
 </template>
@@ -32,6 +27,7 @@ import axios from 'axios'
 import Datepicker from '@vuepic/vue-datepicker'
 import '@vuepic/vue-datepicker/dist/main.css'
 import { ref } from 'vue';
+// import { stringify } from 'querystring';
 
 export default {
   props: {
@@ -45,27 +41,37 @@ export default {
   },
   setup() {
     const date = ref();
-    const textInputOptions = ref({
-      format: 'yyyy.MM.dd',
-    });
     const time = ref({
       hours: new Date().getHours(),
       minutes: new Date().getMinutes(),
     });
-    const alertDate = () => {
-          alert(date.value);
-        };
+    const format = (date) => {
+      var dataset = [];
+      var strTime = '';
+      date.forEach(el => {
+        dataset.push(el.getFullYear() + '.' + (el.getMonth() + 1) + '.' + el.getDate());
+        strTime =  el.getHours() + ':' + el.getMinutes();
+      });
+      console.log('stringify', JSON.stringify(dataset));
+      return `${JSON.stringify(dataset)} ${strTime}` ;
+      // const day = date[1].getDate();
+      // const month = date[1].getMonth() + 1;
+      // const year = date[1].getFullYear();
+      // console.log('1time: ' ,date[0].getHours(), date[0].getMinutes());
 
+      // return `Selected ${year}.${month}.${day}`;
+    };
     return {
       date,
-      textInputOptions,
       time,
-      alertDate,
+      format,
     }
+
   },
   methods: {
+
     clickParams: async function () {
-      console.log("textInputOptions ", this.textInputOptions );
+      console.log("this.date ", this.date);
       var resdata;
       var per_resdata;
       await axios.get('server/get_chart_data2.jsp').then((response) => {
@@ -75,8 +81,8 @@ export default {
       await axios.get('server/get_chart_data.jsp').then((response) => {
         per_resdata = response.data.split('%>')[1].substring(2);
       });
-      console.log('resdata: ' + resdata);
-      console.log('per_resdata: ' + per_resdata);
+      // console.log('resdata: ' + resdata);
+      // console.log('per_resdata: ' + per_resdata);
       this.$router.push({
         // name: "ChartView",
         name: "DashBoard",
@@ -85,7 +91,12 @@ export default {
           per_jsondata: per_resdata
         },
       });
+      // var lstdate[] = this.date.
+      console.log("date>", this.date)
     }
+  },
+  mounted() {
+
   }
 
 }
@@ -109,5 +120,12 @@ li {
 
 a {
   color: #42b983;
+}
+</style>
+
+<style>
+.dp__input_wrap {
+    width: 380px;
+    height: 38px;
 }
 </style>
